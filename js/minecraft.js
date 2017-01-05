10a66c8c9c300a1e621dc4a4c67be23ad605b783
biko004 <Biko004@gmail.com>
2017-01-05 16:31:50 +0200

    BIG CHANGE!

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,38 +1,66 @@
 var Minecraft = {};
 
-Minecraft.startGame = function(){
+Minecraft.startGame = function () {
     $("#intro").css("display", "none");
 };
 
 //update the temp picked element
-Minecraft.tempElement = function(tempPicked){
-    $('#emptyBoxtool').removeClass()
-                    .css({"display" : "block"})
-                    .addClass(tempPicked);
+Minecraft.tempElement = function (tempPicked) {
+    $('#inventory')
+        .css({"border": "1px solid white"})
+        .addClass(tempPicked);
 }
 
 // box is clicked
-Minecraft.boxClicked = function(){
+Minecraft.boxClicked = function () {
     var line = $(this).data("line");
     var col = $(this).data("column");
-    console.log("  line:" + line + "  column:" +col );
 
-    if(Minecraft.selectedTool=="axetool"){
-        if(Minecraft.matrix[line][col] =="tree" || Minecraft.matrix[line][col] =="leaf"){
+    console.log("  line:" + line + "  column:" + col);
+
+    if (Minecraft.selectedTool == "axetool") {
+        if (Minecraft.matrix[line][col] == "tree" || Minecraft.matrix[line][col] == "leaf") {
+            console.log(Minecraft.matrix[line][col]);
             Minecraft.tempElement(Minecraft.matrix[line][col]);
+            Minecraft.updateCounter(Minecraft.matrix[line][col]);
             Minecraft.matrix[line][col] = "";
+
+        }
+
+        else {
+            $("#axetool").css("background-color", "red");
+
+            setTimeout(function () {
+                $("#axetool").css("background-color", "blue");
+            }, 200);
         }
     }
-    else if(Minecraft.selectedTool=="pickAxeTool"){
-        if(Minecraft.matrix[line][col] =="rock"){
+    else if (Minecraft.selectedTool == "pickAxeTool") {
+        if (Minecraft.matrix[line][col] == "rock") {
             Minecraft.tempElement(Minecraft.matrix[line][col]);
+            Minecraft.updateCounter(Minecraft.matrix[line][col]);
             Minecraft.matrix[line][col] = "";
+
+        }
+        else {
+            $("#pickAxeTool").css("background-color", "red");
+            setTimeout(function () {
+                $("#pickAxeTool").css("background-color", "blue");
+            }, 200);
         }
     }
-    else if(Minecraft.selectedTool=="shovelTool"){
-        if(Minecraft.matrix[line][col] =="dirt" || Minecraft.matrix[line][col] =="grass" ){
+    else if (Minecraft.selectedTool == "shovelTool") {
+        if (Minecraft.matrix[line][col] == "dirt" || Minecraft.matrix[line][col] == "grass") {
             Minecraft.tempElement(Minecraft.matrix[line][col]);
+            Minecraft.updateCounter(Minecraft.matrix[line][col]);
             Minecraft.matrix[line][col] = "";
+
+        }
+        else {
+            $("#shovelTool").css("background-color", "red");
+            setTimeout(function () {
+                $("#shovelTool").css("background-color", "blue");
+            }, 200);
         }
     }
 
@@ -42,21 +70,21 @@ Minecraft.boxClicked = function(){
 
 
 Minecraft.createBoard = function () {
-    
+
     //creates matrix array
     Minecraft.matrix = new Array(20);
-    for(var l=0; l<Minecraft.matrix.length; l++){
+    for (var l = 0; l < Minecraft.matrix.length; l++) {
         Minecraft.matrix[l] = new Array(20);
     }
-    for(var x=0; x<Minecraft.matrix.length; x++){
-        for(var y=0; y<Minecraft.matrix.length; y++){
+    for (var x = 0; x < Minecraft.matrix.length; x++) {
+        for (var y = 0; y < Minecraft.matrix.length; y++) {
             Minecraft.matrix[x][y] = "";
         }
     }
 
 
 /// create board
-    for(var i=0; i<Minecraft.matrix.length; i++){
+    for (var i = 0; i < Minecraft.matrix.length; i++) {
         for (var j = 0; j < Minecraft.matrix[i].length; j++) {
             var box = $("<div/>")
                 .addClass("box")
@@ -70,66 +98,67 @@ Minecraft.createBoard = function () {
 
 Minecraft.createBoard();
 
-Minecraft.updateBoard = function(){
+Minecraft.updateBoard = function () {
     Minecraft.boxes
         .removeClass("grass")
         .removeClass("dirt")
         .removeClass("tree")
+        .removeClass("leaf")
         .removeClass("rock");
 
-    for(var i=0; i<Minecraft.matrix.length; i++){
+    for (var i = 0; i < Minecraft.matrix.length; i++) {
         for (var j = 0; j < Minecraft.matrix[i].length; j++) {
-            Minecraft.boxes.eq(i*20 + j).addClass(Minecraft.matrix[i][j]);
+            Minecraft.boxes.eq(i * 20 + j).addClass(Minecraft.matrix[i][j]);
         }
     }
 }
 
 Minecraft.boxes = $(".box");
 
-Minecraft.createTree = function(line, column){
+Minecraft.createTree = function (line, column) {
     Minecraft.matrix[line][column] = "tree";
-    Minecraft.matrix[line-1][column] = "tree";
-    Minecraft.matrix[line-2][column] = "tree";
+    Minecraft.matrix[line - 1][column] = "tree";
+    Minecraft.matrix[line - 2][column] = "tree";
 
 
-    Minecraft.matrix[line-3][column] = "leaf";
-    Minecraft.matrix[line-4][column] = "leaf";
-    Minecraft.matrix[line-5][column] = "leaf";
+    Minecraft.matrix[line - 3][column] = "leaf";
+    Minecraft.matrix[line - 4][column] = "leaf";
+    Minecraft.matrix[line - 5][column] = "leaf";
 
-    Minecraft.matrix[line-3][column+1] = "leaf";
-    Minecraft.matrix[line-4][column+1] = "leaf";
-    Minecraft.matrix[line-5][column+1] = "leaf";
+    Minecraft.matrix[line - 3][column + 1] = "leaf";
+    Minecraft.matrix[line - 4][column + 1] = "leaf";
+    Minecraft.matrix[line - 5][column + 1] = "leaf";
 
-    Minecraft.matrix[line-3][column-1] = "leaf";
-    Minecraft.matrix[line-4][column-1] = "leaf";
-    Minecraft.matrix[line-5][column-1] = "leaf";
+    Minecraft.matrix[line - 3][column - 1] = "leaf";
+    Minecraft.matrix[line - 4][column - 1] = "leaf";
+    Minecraft.matrix[line - 5][column - 1] = "leaf";
 
 
 };
-Minecraft.createRock = function(line, column){
+Minecraft.createRock = function (line, column) {
     Minecraft.matrix[line][column] = "rock";
 };
-Minecraft.createCloud = function(line, column) {
+Minecraft.createCloud = function (line, column) {
     Minecraft.matrix[line][column] = "cloud";
 
 };
-Minecraft.createBush = function(line, column) {
+Minecraft.createBush = function (line, column) {
     Minecraft.matrix[line][column] = "leaf";
-    Minecraft.matrix[line-1][column] = "leaf";
-    Minecraft.matrix[line][column+1] = "leaf";
-    Minecraft.matrix[line][column-1] = "leaf";
+    Minecraft.matrix[line - 1][column] = "leaf";
+    Minecraft.matrix[line][column + 1] = "leaf";
+    Minecraft.matrix[line][column - 1] = "leaf";
 
 };
 
-Minecraft.drawBoard = function(){
+Minecraft.drawBoard = function () {
 
     // Draw Dirt and Grass
-    for(var i=15; i<Minecraft.matrix.length; i++){
+    for (var i = 15; i < Minecraft.matrix.length; i++) {
         for (var j = 0; j < Minecraft.matrix[i].length; j++) {
-            if(i === 15){
+            if (i === 15) {
                 Minecraft.matrix[i][j] = "grass";
             }
-            else{
+            else {
                 Minecraft.matrix[i][j] = "dirt";
             }
         }
@@ -165,45 +194,97 @@ Minecraft.drawBoard = function(){
     Minecraft.updateBoard();
 };
 Minecraft.drawBoard();
-
-
-
+var dirtCounter = 0;
+var grassCounter = 0;
+var treeCounter = 0;
+var leafCounter = 0;
+var rockCounter = 0;
 
 Minecraft.sideBar = function () {
+
+
     var mainMenu = $("<div/>");
     $(mainMenu).addClass("menuClass");
-    
+
     var pickAxe = $("<div/>")
-            .addClass("tool")
-            .attr("id", "pickAxeTool");
+        .addClass("tool")
+        .attr("id", "pickAxeTool");
     var pickTitle = $("<p/>")
-            .text("PickAxe")
-            .addClass("textClass");
+        .text("PickAxe")
+        .addClass("textClass");
     $(pickAxe).append(pickTitle);
     $(mainMenu).append(pickAxe);
 
     var shovel = $("<div/>")
-            .addClass("tool")
-            .attr("id", "shovelTool");
+        .addClass("tool")
+        .attr("id", "shovelTool");
     var shovelTitle = $("<p/>")
-            .text("Shovel")
-            .addClass("textClass");
+        .text("Shovel")
+        .addClass("textClass");
     $(shovel).append(shovelTitle);
     $(mainMenu).append(shovel);
 
     var axe = $("<div/>")
-            .addClass ("tool")
-            .attr("id", "axetool");
+        .addClass("tool")
+        .attr("id", "axetool");
     var axeTitle = $("<p/>")
-            .text("Axe")
-            .addClass("textClass");
+        .text("Axe")
+        .addClass("textClass");
     $(axe).append(axeTitle);
-    $(mainMenu).append(axe); 
+    $(mainMenu).append(axe);
 
-    var emptyBox = $("<div/>")
-            .addClass("emptyBoxClass")
-            .attr("id", "emptyBoxtool");
-    $(mainMenu).append(emptyBox);
+
+
+
+    var tempDirt = $("<div/>")
+        .addClass("inventory")
+        .attr("id", "dirt");
+    var dirtCount = $("<p/>")
+        .text(dirtCounter)
+        .attr("id", "dirtCount")
+        .addClass("counters");
+    $(tempDirt).append(dirtCount);
+    $(mainMenu).append(tempDirt);
+
+    var tempGrass = $("<div/>")
+        .addClass("inventory")
+        .attr("id", "grass");
+    var grassCount = $("<p/>")
+        .text(grassCounter)
+        .attr("id", "grassCount")
+        .addClass("counters");
+    $(tempGrass).append(grassCount);
+    $(mainMenu).append(tempGrass);
+
+    var tempTree = $("<div/>")
+        .addClass("inventory")
+        .attr("id", "tree");
+    var treeCount = $("<p/>")
+        .text(treeCounter)
+        .attr("id", "treeCount")
+        .addClass("counters");
+    $(tempTree).append(treeCount);
+    $(mainMenu).append(tempTree);
+
+    var tempLeaf = $("<div/>")
+        .addClass("inventory")
+        .attr("id", "leaf");
+    var leafCount = $("<p/>")
+        .text(leafCounter)
+        .attr("id", "leafCount")
+        .addClass("counters");
+    $(tempLeaf).append(leafCount);
+    $(mainMenu).append(tempLeaf);
+
+    var tempRock = $("<div/>")
+        .addClass("inventory")
+        .attr("id", "rock");
+    var rockCount = $("<p/>")
+        .text(rockCounter)
+        .attr("id", "rockCount")
+        .addClass("counters");
+    $(tempRock).append(rockCount);
+    $(mainMenu).append(tempRock);
 
     $("body").append(mainMenu);
 };
@@ -211,17 +292,48 @@ Minecraft.sideBar = function () {
 Minecraft.sideBar();
 
 
-Minecraft.toolSelect = function(){
-    if($(this).hasClass("tool")){
+Minecraft.toolSelect = function () {
+
+    if ($(this).hasClass("tool")) {
+        $(".tool").css("background-color", "black");
+        $(this).css("background-color", "blue");
         Minecraft.selectedTool = $(this).attr('id');
         console.log("Current tool: " + Minecraft.selectedTool);
     }
-    else{
-        Minecraft.selectedTool = 'tempTool';
+    else if ($(this).hasClass("inventory")) {
+        Minecraft.selectedTool = $(this).attr('id');
+        console.log("Current temp selected: " + Minecraft.selectedTool);
     }
 };
 
-$(".tool").on("click",Minecraft.toolSelect);
+$(".tool").on("click", Minecraft.toolSelect);
+
+$(".inventory").on("click", Minecraft.toolSelect);
+
+Minecraft.updateCounter = function(tempElement){
+    if(tempElement == "tree"){
+        treeCounter++
+    }
+    else if(tempElement == "leaf"){
+        leafCounter++
+    }
+    else if(tempElement == "rock"){
+        rockCounter++
+    }
+    else if(tempElement == "grass"){
+        grassCounter++
+    }
+    else if(tempElement == "dirt"){
+        dirtCounter++
+    }
+
+    $("#treeCount").text(treeCounter);
+    $("#leafCount").text(leafCounter);
+    $("#rockCount").text(rockCounter);
+    $("#grassCount").text(grassCounter);
+    $("#dirtCount").text(dirtCounter);
+
+};
 
 
 
